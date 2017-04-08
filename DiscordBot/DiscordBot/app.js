6af5dd71e61e95cecbe7c4137cd3dfb5c5cd494b
Seth Heno <fearmyheno@gmail.com>
2017-04-07 22:21:50 -0400

    Real Initial Commit

--- DiscordBot/DiscordBot/app.js
+++ DiscordBot/DiscordBot/app.js
@@ -1,3 +1,49 @@
-'use strict';
+/*
+  A ping pong bot, whenever you send "ping", it replies "pong".
+*/
 
-console.log('Hello world');
+// import the discord.js module
+const Discord = require('discord.js');
+
+// create an instance of a Discord Client, and call it bot
+const bot = new Discord.Client();
+
+// the token of your bot - https://discordapp.com/developers/applications/me
+const token = '';
+
+var loveCharlie = 0;
+
+// the ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted.
+bot.on('ready', () => {
+    console.log('I am ready!');
+});
+
+bot.on('message', message => {
+    console.log(message.author);
+    if (message.content.toUpperCase() === 'HELP') {
+        message.reply('Hello! I have the following commands available \n');
+        message.reply('\t Contribute to the Charlie harem. Increment the counter with \'I love Charlie\' \n');
+    }
+});
+
+// create an event listener for messages
+bot.on('message', message => {
+    // if the message is "ping",
+    if (message.content.toLowerCase() === 'i love charlie') {
+        loveCharlie++;
+        message.channel.sendMessage('Charlie has been loved: ' + loveCharlie + ' times');
+    }
+});
+
+// create an event listener for messages
+bot.on('message', message => {
+    // if the message is "what is my avatar",
+    if (message.content === 'what is my avatar') {
+        // send the user's avatar URL
+        message.reply(message.author.avatarURL);
+    }
+});
+
+// log our bot in
+bot.login(token);
