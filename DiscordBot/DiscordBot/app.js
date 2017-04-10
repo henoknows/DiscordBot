bf68e58cb536c27a72a1535e3313eccbf9126c19
Seth Heno <fearmyheno@gmail.com>
2017-04-10 19:16:50 -0400

    Reorgainze methods to single  message listener

--- DiscordBot/DiscordBot/app.js
+++ DiscordBot/DiscordBot/app.js
@@ -1,9 +1,12 @@
 /*
-  A ping pong bot, whenever you send "ping", it replies "pong".
+ A crappy implementation of a discord bot
 */
 
 // import the discord.js module
 const Discord = require('discord.js');
+
+// import filesystem api
+const fs = require('fs');
 
 // create an instance of a Discord Client, and call it bot
 const bot = new Discord.Client();
@@ -11,7 +14,55 @@ const bot = new Discord.Client();
 // the token of your bot - https://discordapp.com/developers/applications/me
 const token = '';
 
-var loveCharlie = 0;
+// Number of images in 'DankMemes'
+var dankMemesNum = 3;
+var min = 1;
+
+// Generate a random number for dank meme user
+function getRandomInt() {
+    return Math.floor(Math.random() * (dankMemesNum - min)) + min;
+}
+
+// Read file for lover/hate counter
+function updateFile(message, name, incrementType) {
+    var counter = 0;
+    var filePath = 'RoleInts/' + name + '.txt';
+    console.log('Fail here ' + incrementType);
+    try {
+        counter = fs.readFileSync(filePath);
+    } catch (err) {
+        console.log('File does not exist. Create one');
+        fs.openSync(filePath, 'w');
+        counter = 0;
+    }
+
+    if (incrementType === 'a') {
+        console.log(message.author.username);
+        if (message.author.username.toLowerCase().includes(name)) {
+            counter--; // A little hacky but works.
+            message.channel.sendMessage('How sad, this is the only way you can get points...');
+        }
+        counter++;
+        message.channel.sendMessage(name + '\'s sexy meter: ' + counter + '');
+    }
+
+    else if (incrementType === 's') {
+        counter--;
+        message.channel.sendMessage('Yeah that baka! ' + name + '\'s sexy meter: ' + counter + '');
+    }
+
+    else if (incrementType === '2s') {
+        counter -= 2;
+        message.channel.sendMessage('Yeah that baka! ' + name + '\'s sexy meter: ' + counter + '');
+    }
+
+    var options = { flag: 'w' };
+    fs.writeFile(filePath, counter, options, function (err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
+}
+
 
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
@@ -19,31 +70,43 @@ bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on('message', message => {
-    console.log(message.author);
-    if (message.content.toUpperCase() === 'HELP') {
-        message.reply('Hello! I have the following commands available \n');
-        message.reply('\t Contribute to the Charlie harem. Increment the counter with \'I love Charlie\' \n');
-    }
-});
-
 // create an event listener for messages
 bot.on('message', message => {
+
     // if the message is "ping",
-    if (message.content.toLowerCase() === 'i love charlie') {
-        loveCharlie++;
-        message.channel.sendMessage('Charlie has been loved: ' + loveCharlie + ' times');
-    }
-});
+    var input = message.content.toLowerCase();
+    var inputArray = input.split(' ');
 
-// create an event listener for messages
-bot.on('message', message => {
-    // if the message is "what is my avatar",
-    if (message.content === 'what is my avatar') {
+    // Help Commands
+    if (message.content.toLowerCase() === '!help') {
+        message.channel.sendMessage('Hello! I have the following commands available \n');
+        message.channel.sendMessage('\t Contribute to someone\'s harem with \'!love [name]\'');
+        message.channel.sendMessage('\t\t Unless you hate them. \'!hate charlie\' or \'!fuck charlie\' \n');
+    }
+
+    else if (inputArray[0].includes('!love')) {
+        console.log(inputArray[1]);
+        updateFile(message, inputArray[1], 'a');
+    }
+
+    else if (inputArray[0].includes('!hate')) {
+        updateFile(message, inputArray[1], 's');
+    }
+
+    else if (inputArray[0].includes('!fuck')) {
+        updateFile(message, inputArray[1], '2s');
+    }
+
+    else if (input.includes('dank meme')) {
+        message.channel.sendMessage("", { file: "DankMemes/" + getRandomInt() + ".jpg" });
+    }
+
+    else if (message.content.toLowerCase() === 'what is my avatar') {
         // send the user's avatar URL
         message.reply(message.author.avatarURL);
     }
 });
+
 
 // log our bot in
 bot.login(token);
